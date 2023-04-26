33a40f1da6110d4765cd28e3cffb4682fabfab16
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-04-27 00:21:13 +0300

    Process all text as one 2d tensor, print in CSV

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 from typing import Dict
 
+import pandas as pd
 import torch
 from transformers import RobertaTokenizer, RobertaModel
 import wikipedia
-import model_of_GPT
-
-name_of_article = "Elvis_Presley"
 
 """
 This code graps Elvis Presley page from Wikipedia and weeds in to RoBERTa to get token embeddings
+Prints acquired data into embeddings.txt file
 """
 
 
-def parse_wiki(title: str = name_of_article) -> str:
+def parse_wiki(title: str = "Elvis_Presley") -> str:
     wikipedia.set_lang("en")
 
     target_page = wikipedia.page(title)
@@ -27,38 +26,28 @@ Prints acquired data into embeddings.txt file
 
 def main():
     model_name = 'roberta-base'
-
     tokenizer = RobertaTokenizer.from_pretrained(model_name)
     model = RobertaModel.from_pretrained(model_name)
 
-    # encoded_input = tokenizer(text, return_tensors='pt')
-    # output = model(**encoded_input)
+    text = parse_wiki()
 
-    tokens = tokenizer.tokenize(parse_wiki())
+    # Should we add <s> </s> tags?
+    tokens = tokenizer.tokenize(text)
     input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-    tensor_len: int = 512
-    count: int = 1 + len(input_ids) // tensor_len
-    print(f"Text is divided into {count} parts")
+    vector_len: int = 512
+    padding_len: int = vector_len - (len(input_ids) % vector_len)
+    input_ids += [tokenizer.pad_token_id] * padding_len  # add padding
 
-    with open('embeddings.txt', 'bw+') as f:
-        for i in range(count):
-            print(f"{i + 1} / {count}...", end='')
-            tokens_slice = tokens[i:i + tensor_len]
-            input_ids_slice = input_ids[i:i + tensor_len]
+    input_ids_tensor = torch.tensor(input_ids).reshape((-1, vector_len))
+    output = model(input_ids_tensor)
 
-            input_ids_tensor = torch.tensor(input_ids_slice).unsqueeze(0)
-            output = model(input_ids_tensor)
+    embeddings = output.last_hidden_state.detach()
+    embeddings = embeddings.reshape((-1, embeddings.size()[2]))  # Squeeze batch dimension
 
-            embeddings = output.last_hidden_state.squeeze(0)
-
-            for t, e in zip(tokens_slice, embeddings):
-                f.write(f"{t}:\n".encode('utf-8'))
-                f.write(f"{e!r}\n\n".encode('utf-8'))
-
-            print("done")
+    df = pd.DataFrame(embeddings)
+    df.to_csv('embeddings.csv')
 
 
 if __name__ == '__main__':
     main()
-    print(model_of_GPT.model(name_of_article))
