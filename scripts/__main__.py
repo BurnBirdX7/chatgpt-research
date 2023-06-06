71724c88d76c94577988c4a1b4c8a38274642906
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-06-06 16:14:50 +0300

    Added scripts module

---
+++ scripts/__main__.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        raise ValueError("Not enough parameters")
+
+    script = f"scripts/{sys.argv[1]}.py"
+    project_root = os.path.dirname(os.path.dirname(__file__))
+
+    args = [sys.executable, script, *sys.argv[2:]]
+
+    env = os.environ.copy()
+    if 'PYHTONPATH' in env:
+        env['PYTHONPATH'] += f";{project_root}"
+    else:
+        env['PYTHONPATH'] = project_root
+
+    subprocess.run(args, cwd=project_root, env=env)
+
+
+if __name__ == '__main__':
+    main()
+
