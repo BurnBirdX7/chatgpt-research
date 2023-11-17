c53036b2f134f93cc75e3674e2ce72a0e6d073f8
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-11-17 21:46:37 +0300

    Added FEVER to ColBERT format conversion

---
+++ colbert/install_colbert.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Clone colbert
+git clone https://github.com/stanford-futuredata/ColBERT.git colbert
+
+# Download and unpack ColBERTv2 checkpoint
+wget https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz -O checkpoint.tar.gz
+tar -xvzf checkpoint.tar.gz
+mv colbertv2.0 checkpoint
+
+# Download FEVER dataset
+wget https://fever.ai/download/fever/train.jsonl -P collections/
