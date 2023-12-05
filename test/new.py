fddbd5353b7dde4ab0d15965109a1482ffde3322
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-12-05 22:49:09 +0300

    Sequence generation refactoring, optimization

--- test/new.py
+++ test/new.py
@@ -1,5 +1,8 @@
+import copy
 import math
 import time
+from typing import Tuple, List, Set, Optional
+
 import numpy as np  # type: ignore
 import faiss  # type: ignore
 import wikipediaapi  # type: ignore
@@ -13,9 +16,7 @@ tokenizer, model = Roberta.get_default()
 modelMLM = RobertaForMaskedLM.from_pretrained('roberta-large')
 batched_token_ids = torch.empty((1, 512), dtype=torch.int)
 
-result_sequence = []
-
-page_template = """
+page_template_str = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,10 +36,9 @@ page_template = """
 </html>
 """
 
-link_template = "<a href=\"{{ link }}\" class=\"{{ color }}\">{{ token }}</a>"
-link_template_empty = "<a href=\"{{ link }}\" class=\"{{ color }}\"><i>{{ token }}</i></a>"
-list_of_articles = "<a href=\"{{ link }}\" class=\"{{ color }}\">{{ token }}</a></br>"
-
+source_link_template_str = "<a href=\"{{ link }}\" class=\"{{ color }}\">{{ token }}</a>"
+source_text_template_str = "<a class=\"{{ color }}\"><i>{{ token }}</i></a>"
+source_item_str = "<a href=\"{{ link }}\" class=\"{{ color }}\">{{ token }}</a></br>"
 
 
 def get_page_section_from_wiki(source: str) -> str:
@@ -55,18 +55,62 @@ def get_page_section_from_wiki(source: str) -> str:
         section = target_page.summary
     else:
         section = target_page.section_by_title \
-        (source.split("https://en.wikipedia.org/wiki/")[1].split("#")[1].replace("_"," "))  # возврощает последнюю секцию
+            (source.split("https://en.wikipedia.org/wiki/")[1].split("#")[1].replace("_",
+                                                                                     " "))  # возврощает последнюю секцию
 
     return str(section)
 
 
-def make_chain_colored_recursive_right(new_chain, last_hidden_state, hidden_state_start, tokens, token_pos, result_ids,
-                                       chain_count):
-    for hidden_state in range(hidden_state_start, 512):
-        continue_chain = new_chain.copy()
-        probs = torch.nn.functional.softmax(last_hidden_state[hidden_state])
+class Chain:
+    string: str
+    likelihoods: List[float]
+    positions: List[int]
+    source: str
 
-        column_tokens = torch.topk(last_hidden_state[hidden_state], k=20, dim=0)
+    def __init__(self, string: str, likelihoods: List[float], positions: List[int], source: str):
+        assert (len(likelihoods) == len(positions))
+        self.string = string
+        self.likelihoods = likelihoods
+        self.positions = positions
+        self.source = source
+
+    def __len__(self) -> int:
+        return len(self.positions)
+
+    def extend(self, string: str, likelihood: float, position: int) -> "Chain":
+        return Chain(self.string + string,
+                     self.likelihoods + [likelihood],
+                     self.positions + [position],
+                     self.source)
+
+    def get_score(self):
+        l = len(self)
+
+        # log2(2 + len) * ((lik_h_0 * ... * lik_h_len) ^ 1 / len) - score
+        score = 1.0
+        for lh in self.likelihoods:
+            score *= lh
+
+        score **= 1 / l
+        score *= math.log2(2 + l)
+        return score
+
+
+# GLOBAL result sequence:
+result_sequence: List[Chain] = []
+
+
+def generate_sequences(chain: Chain, last_hidden_state: torch.Tensor, start_idx: int, tokens: List[str], token_pos):
+
+    if start_idx >= len(last_hidden_state) or token_pos >= len(tokens):
+        if len(chain) > 1:
+            result_sequence.append(chain)
+
+        return
+
+    for idx in range(start_idx, len(last_hidden_state)):
+        probs = torch.nn.functional.softmax(last_hidden_state[idx])
+        column_tokens = torch.topk(last_hidden_state[idx], k=20, dim=0)
         top_twenty_tokens = column_tokens[1]
         probability_top_twenty_tokens = [probs[i] for i in top_twenty_tokens]
         words = [tokenizer.decode(i.item()).strip() for i in top_twenty_tokens]
@@ -75,76 +119,17 @@ def make_chain_colored_recursive_right(new_chain, last_hidden_state, hidden_stat
         if "Ġ" in token:
             token = token.split("Ġ")[1]
 
-        for word in range(len(words)):
-            if words[word] == token and probability_top_twenty_tokens[word].item() >= 0.1:
-                chain_count += 1
-
-                # log2(2+len)*((lik_h_0*...*lik_h_len)^1/len) - score
-                # score = math.log2(2 + chain_count)*math.pow(continue_chain[1]*probability_top_twenty_tokens[word].item(),
-                #                                         1/chain_count)
-                # len*((lik_h_0*...*lik_h_len)^1/len) - score
-                # score = chain_count*math.pow(continue_chain[1]*probability_top_twenty_tokens[word].item(),
-                #                                         1/chain_count)
-                continue_chain[0] = continue_chain[0]+token
-                probability_token_in_chain = continue_chain[1].copy()
-                probability_token_in_chain.append(probability_top_twenty_tokens[word].item())
-                continue_chain[1] = probability_token_in_chain
-                # continue_chain[2]=str(continue_chain[2])+"|"+str(result_ids[token_pos].item())
-                tokens_in_chain = continue_chain[2].copy()
-                tokens_in_chain.append(token_pos)
-                continue_chain[2] = tokens_in_chain
-
-                # continue_chain.append(token)
-                # continue_chain.append(probability_top_twenty_tokens[word].item())
-                # continue_chain.append(result_ids[token_pos].item())
-
-                if hidden_state != len(last_hidden_state) - 1 and token_pos != len(tokens) - 1:
-                    make_chain_colored_recursive_right(continue_chain, last_hidden_state, hidden_state + 1, tokens,
-                                                   token_pos + 1, result_ids, chain_count)
+        for word_idx in range(len(words)):
+            if words[word_idx] == token:
+                prob = probability_top_twenty_tokens[word_idx].item()
+                current_chain = chain.extend(token, prob, token_pos)
+                if prob >= 0.1:
+                    generate_sequences(current_chain, last_hidden_state, idx + 1,
+                                       tokens, token_pos + 1)
                 else:
-                    if chain_count > 1:
-                        multy = 1
-                        for probability in range(len(continue_chain[1])):
-                            multy = multy * continue_chain[1][probability]
-                        score = math.log2(2 + chain_count) * math.pow(multy, 1 / chain_count)
-                        continue_chain[1]=score
-                        result_sequence.append(continue_chain)
-            else:
-                if chain_count > 1:
-                    multy = 1
-                    for probability in range(len(continue_chain[1])):
-                        multy = multy * continue_chain[1][probability]
-                    score = math.log2(2 + chain_count) * math.pow(multy, 1 / chain_count)
-                    continue_chain[1] = score
-                    result_sequence.append(continue_chain)
+                    if len(current_chain) > 1:
+                        result_sequence.append(current_chain)
 
-
-
-
-
-def iterate_on_sorted_sequence(iterator_sorting, filtered_elements):
-    filtered_elements_new = []
-    for i in range(iterator_sorting):
-        filtered_elements_new.append(filtered_elements[i])
-
-    first_elements = filtered_elements[iterator_sorting]
-    for i, chain in enumerate(filtered_elements):
-        if i > iterator_sorting:
-            flag = False
-            for j, seq_item in enumerate(chain[2]):
-                for k, first_item in enumerate(first_elements[2]):
-                    if seq_item == first_item:
-                        flag = True
-            if flag is False:
-                filtered_elements_new.append(filtered_elements[i])
-
-
-    print("res_first", filtered_elements_new)
-    if iterator_sorting<len(filtered_elements_new)-1:
-        iterator_sorting+=1
-        iterate_on_sorted_sequence(iterator_sorting, filtered_elements_new)
-
-    return
 
 def cast_output(tokens, source_link):
     for key, src in enumerate(tokens):
@@ -154,14 +139,13 @@ def cast_output(tokens, source_link):
     for i, key, src in enumerate(zip(tokens, source_link)):
         print("::", i, "::", key, "::", src)
 
-    template = Template(link_template)
+    template = Template(source_link_template_str)
     output = ''
     for i, key in enumerate(tokens):
         value_from_map1 = tokens[key]
         value_from_map2 = source_link[key]
         print(value_from_map1, value_from_map2)
         # Check if the key is present in the second map
-
 
 
 def main(gpt_response) -> None:
@@ -191,135 +175,86 @@ def main(gpt_response) -> None:
 
         for batch in range(0, token_ids.shape[1], 511):
             batched_token_ids = token_ids[0, batch:batch + 512].unsqueeze(0)
+
             with torch.no_grad():
-                output = modelMLM(batched_token_ids)
+                output_page = modelMLM(batched_token_ids)
 
-            last_hidden_state = output[0].squeeze()
-            print("res:::", last_hidden_state)
-            print("shape:::", len(last_hidden_state))
+            last_hidden_state = output_page[0].squeeze()
 
-            for hidden_state in range(len(last_hidden_state)):
-                local_sequence = []
-
-                probs = torch.nn.functional.softmax(last_hidden_state[hidden_state])
-                column_tokens = torch.topk(last_hidden_state[hidden_state], k=20, dim=0)
-                top_twenty_tokens = column_tokens[1]
-
-                probability_top_twenty_tokens = [probs[i] for i in top_twenty_tokens]
-                words = [tokenizer.decode(i.item()).strip() for i in top_twenty_tokens]
-
-                tokens_in_chains_prob = []
-                for word in range(len(words)):
-                    token_pos_in_chain = []
-                    if words[word] == token and probability_top_twenty_tokens[word].item() >= 0.1:
-                        print("first:first:::", top_twenty_tokens)
-                        print("ffff:", probability_top_twenty_tokens)
-                        print("ffffftt::", words)
-                        chain_count = 1
-                        local_sequence.append(token)
-                        local_sequence.append(tokens_in_chains_prob)
-                        local_sequence[1].append(probability_top_twenty_tokens[word].item())
-                        # local_sequence.append(probability_top_twenty_tokens[word].item())  # first score
-                        # local_sequence.append(result_ids[token_pos].item())
-
-                        token_pos_in_chain.append(token_pos)
-                        local_sequence.append(token_pos_in_chain)
-                        local_sequence.append(source)
-                        print("first::", local_sequence, "::::", result_ids[token_pos].item())
-
-                        # result_sequence.append(local_sequence)
-
-                        if hidden_state != len(last_hidden_state) - 1 and token_pos != len(tokens) - 1:
-                            make_chain_colored_recursive_right(local_sequence, last_hidden_state, hidden_state + 1,
-                                                               tokens, token_pos + 1, result_ids, chain_count)
+            empty_chain = Chain("", [], [], source)
+            generate_sequences(empty_chain, last_hidden_state, 0, tokens, token_pos)
 
             print("probe res::", result_sequence)
 
     print("whole res::", result_sequence)
     print("shape_of_end_sequence:::", len(result_sequence))
 
-    sorted_result_sequence = sorted(result_sequence, key=lambda x: x[1], reverse=True)
-    print("sorting:::", sorted_result_sequence)
+    sorted_result_chain = sorted(result_sequence, key=lambda x: x.get_score(), reverse=True)
+    print("sorting:::", sorted_result_chain)
 
-    filtered_elements = []
-    iterator_sorting = 1
-    first_elements = sorted_result_sequence[0]
+    filtered_chains: List[Chain] = []
+    marked_positions: Set[int] = set()
+    for chain in sorted_result_chain:
+        marked_in_chain = marked_positions.intersection(chain.positions)
+        if len(marked_in_chain) == 0:
+            marked_positions |= set(chain.positions)
+            filtered_chains.append(chain)
 
-    filtered_elements.append(first_elements)
-    for i, chain in enumerate(sorted_result_sequence):
-        if i != 0:
-            flag=False
-            for j, seq_item in enumerate(chain[2]):
-                for k, first_item in enumerate(first_elements[2]):
-                    if seq_item == first_item:
-                        flag=True
-            if flag is False:
-                filtered_elements.append(sorted_result_sequence[i])
-
-    print("res_first", filtered_elements)
-    if iterator_sorting < len(filtered_elements)-1:
-        iterator_sorting+=1
-        iterate_on_sorted_sequence(iterator_sorting, filtered_elements)
-
-    print("whole sequence:", filtered_elements)
+    print("whole sequence:", filtered_chains)
     print("whole time:", time.perf_counter() - start)
 
+    # prepare tokens for coloring
+    tokens_for_coloring = map(lambda s: s.replace('Ġ', ' ').replace('Ċ', '</br>'), tokens)
 
-    # prepare tokens for colored
-    tokens_for_colored = map(lambda s: s.replace('Ġ', ' ').replace('Ċ', '</br>'), tokens)
+    # prepare links for coloring
+    pos2source = {}
+    for i, chain in enumerate(filtered_chains):
+        for pos in chain.positions:
+            print(f"pos {pos}, source {chain.source}")
+            pos2source[pos] = chain.source
 
-    # prepare links for colored
-    result_map_sequence_links = {}
+    template_page = Template(page_template_str)
+    template_link = Template(source_link_template_str)
+    template_text = Template(source_text_template_str)
+    template_source_item = Template(source_item_str)
 
-    for i, item in enumerate(filtered_elements):
-        subsequence = item[2]
-        link = item[3]
-        print("link", link)
+    color = 7
+    output_page = ''
+    output_source_list = ''
+    last_source: Optional[str] = None
+    for i, key in enumerate(tokens_for_coloring):
+        key: str
 
-        for number in subsequence:
-            result_map_sequence_links[number] = link
-
-    for key, value in result_map_sequence_links.items():
-        print("mapp:::", key, ":::", value)
-
-    template_res = Template(page_template)
-
-    template = Template(link_template)
-    template_empty = Template(link_template_empty)
-    template_list_of_colors = Template(list_of_articles)
-
-    output = ''
-    output_list_of_colors=''
-    color=7
-    iter=False
-    link_for_colored_per_token=''
-    for i, key in enumerate(zip(tokens_for_colored)):
-        if i in result_map_sequence_links:
-            if link_for_colored_per_token == result_map_sequence_links[i]:
-                output += template.render(link=result_map_sequence_links[i], color="color"+str(color), token=key[0].strip("'"))
+        if i in pos2source:
+            if last_source == pos2source[i]:
+                output_page += template_link.render(link=pos2source[i],
+                                                    color="color" + str(color),
+                                                    token=key[1:-1])
             else:
+                # New sequence:
                 color += 1
-                output_list_of_colors += template_list_of_colors.render(link=result_map_sequence_links[i], color="color"+str(color), token=result_map_sequence_links[i])
-                link_for_colored_per_token = result_map_sequence_links[i]
-                output += template.render(link=result_map_sequence_links[i], color="color"+str(color), token=key[0].strip("'"))
+                output_source_list += template_source_item.render(link=pos2source[i],
+                                                                  color="color" + str(color),
+                                                                  token=pos2source[i])
+                last_source = pos2source[i]
+                output_page += template_link.render(link=pos2source[i],
+                                                    color="color" + str(color),
+                                                    token=key[1:-1])
         else:
-            output += template_empty.render(token=key[0].strip("'"), color="color0")
+            last_source = None
+            output_page += template_text.render(token=key[1:-1], color="color0")
 
-    output_list_of_colors += '</br>'
-    result_html = template_res.render(result=output, gpt_response=gpt_response,  list_of_colors=output_list_of_colors)
+    output_source_list += '</br>'
+    result_html = template_page.render(result=output_page, gpt_response=gpt_response, list_of_colors=output_source_list)
 
     with open("./server/templates/template_of_result_page.html", "w", encoding="utf-8") as f:
         f.write(result_html)
 
-        # print(result_map_sequence_links[key])
-    # cast_output(tokens_for_colored, result_map_sequence_links)
-
-    # print(result_map_sequence)
-
 
 if __name__ == "__main__":
-    main("Presley's father Vernon was of German, Scottish, and English origins, and a descendant of the Harrison family "
-         "of Virginia through his mother, Minnie Mae Presley (née Hood). Presley's mother Gladys was Scots-Irish with "
-         "some French Norman ancestry. She and the rest of the family believed that her great-great-grandmother,"
-         " Morning Dove White, was Cherokee. This belief was restated by Elvis's granddaughter Riley Keough in 2017. "
-         "Elaine Dundy, in her biography, supports the belief.") # gpt output
+    main(
+        "Presley's father Vernon was of German, Scottish, and English origins, and a descendant of the Harrison family "
+        "of Virginia through his mother, Minnie Mae Presley (née Hood). Presley's mother Gladys was Scots-Irish with "
+        "some French Norman ancestry. She and the rest of the family believed that her great-great-grandmother,"
+        " Morning Dove White, was Cherokee. This belief was restated by Elvis's granddaughter Riley Keough in 2017. "
+        "Elaine Dundy, in her biography, supports the belief.")  # gpt output
