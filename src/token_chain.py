8aba445d05d1612458e99313b2499b0104854019
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2024-03-21 03:50:06 +0300

    Moved chain class into a separate file

---
+++ src/token_chain.py
@@ -0,0 +1,72 @@
+import math
+from typing import Optional, List, Set
+
+
+class Chain:
+    begin_pos: Optional[int]
+    end_pos: Optional[int]
+    likelihoods: List[float]
+    skips: int = 0
+    source: str
+
+    def __init__(self, source: str,
+                 begin_pos: Optional[int] = None,
+                 end_pod: Optional[int] = None,
+                 likelihoods: Optional[List[float]] = None,
+                 skips: int = 0):
+        self.begin_pos = begin_pos
+        self.end_pos = end_pod
+        self.likelihoods = [] if (likelihoods is None) else likelihoods
+        self.source = source
+        self.skips = skips
+
+    def __len__(self) -> int:
+        if self.begin_pos is None:
+            return 0
+
+        return self.end_pos - self.begin_pos + 1
+
+    def __str__(self) -> str:
+        return (f"Chain {{\n"
+                f"\tseq = {self.begin_pos}..{self.end_pos}\n"
+                f"\tlikelihoods = {self.likelihoods}\n"
+                f"\tskips = {self.skips}\n"
+                f"\tscore = {self.get_score()}\n"
+                f"\tsource = {self.source}\n"
+                f"}}\n")
+
+    def __repr__(self) -> str:
+        return (f"Chain("
+                f"begin_pos={self.begin_pos}, "
+                f"end_pos={self.end_pos}, "
+                f"likelihoods={self.likelihoods!r}, "
+                f"source={self.source!r}, "
+                f"skips={self.skips}"
+                f")")
+
+    def append(self, likelihood: float, position: int) -> None:
+        self.likelihoods.append(likelihood)
+        if self.begin_pos is None:
+            self.begin_pos = position
+            self.end_pos = position
+        else:
+            if self.end_pos + self.skips + 1 != position:
+                raise ValueError(f"{self.end_pos=}, {position=}")
+            self.end_pos += self.skips + 1
+        self.skips = 0
+
+    def skip(self) -> None:
+        self.skips += 1
+
+    def get_token_positions(self) -> Set[int]:
+        return set(range(self.begin_pos, self.end_pos + 1))
+
+    def get_score(self):
+        # log2(2 + len) * ((lik_h_0 * ... * lik_h_len) ^ 1 / len)   = score
+        score = 1.0
+        for lh in self.likelihoods:
+            score *= lh
+
+        score **= 1 / len(self.likelihoods)
+        score *= math.log2(2 + len(self))
+        return score
