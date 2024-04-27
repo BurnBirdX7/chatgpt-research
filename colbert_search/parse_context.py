9118f2b77873580e78540e6fa01e8946adf0f346
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2024-04-27 07:11:22 +0300

    More concise way to parse wiki dumps

---
+++ colbert_search/parse_context.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import xml.etree.ElementTree as ET
+import typing as t
+import re
+from collections import defaultdict
+
+EventHandler = t.Callable[[ET.Element], None]
+
+_tag_regex = re.compile(r"{(.*)}(\w+)")
+
+class ParseContext:
+
+    def __init__(self, filepath: str):
+        self._iter = ET.iterparse(filepath, events=("start-ns", "end")).__iter__()
+        self._namespace: str | None = None
+        self._start_handlers: t.DefaultDict[str, t.List[EventHandler]] = defaultdict(list)
+        self._end_handlers: t.DefaultDict[str, t.List[EventHandler]] = defaultdict(list)
+
+    def start_handler(self, tag_name: str, handler: EventHandler):
+        self._start_handlers[tag_name].append(handler)
+
+    def end_handler(self, tag_name: str, handler: EventHandler):
+        self._end_handlers[tag_name].append(handler)
+
+    def collect_contents(self, tag_name: str, dest: t.Dict[str, str]):
+        def _handler(elem: ET.Element):
+            dest[tag_name] = elem.text
+
+        self.end_handler(tag_name, _handler)
+
+    def run(self):
+        try:
+            while True:
+                self._next()
+        except StopIteration:
+            return
+
+    def _tag_name(self, elem_name: str) -> str:
+        if self._namespace is None:
+            return elem_name
+        else:
+            m = _tag_regex.match(elem_name)
+            if m is None:
+                return elem_name
+
+            if m.group(1) != self._namespace:
+                return elem_name
+
+            return m.group(2)
+
+    def _next(self):
+        event: str
+        elem : t.Tuple[str, str] | ET.Element
+        event, elem = self._iter.__next__()
+        if event == "start-ns":
+            if elem[0] == "":
+                self._namespace = elem[1]
+            return
+
+        tag_name = self._tag_name(elem.tag)
+
+        if event == "start" and tag_name in self._start_handlers:
+            for handler in self._start_handlers[tag_name]:
+                handler(elem)
+
+        elif event == "end" and tag_name in self._end_handlers:
+            for handler in self._end_handlers[tag_name]:
+                handler(elem)
+
+    def _tag(self, tag_name):
+        return f"{{{self._namespace}}}{tag_name}" if self._namespace is not None else tag_name
--- colbert_search/prepare_wiki.py
+++ colbert_search/prepare_wiki.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 import os.path
 import sys
-from typing import Dict, TextIO, List
+import typing as t
+import urllib.parse
 import xml.etree.ElementTree as ET
 import mwparserfromhell as mw
-from dataclasses import dataclass
 
 from src.pipeline import BaseNode, ListDescriptor
 from src import WikiDataFile, SourceMapping
+
+from parse_context import ParseContext
 
 banned_title_prefixes: list[str] = [
     "Category:",
@@ -26,127 +28,13 @@ def is_title_banned(title: str) -> bool:
 
     return False
 
-
-@dataclass
-class WikiParseContext:
-    # General:
-    collection_name: str = "default"
-    output_dir: str = os.path.abspath("./")
-    namespace: str | None = None
-
-    # Current "session"
-    passage_file: TextIO = None
-    passage_file_path: str = None
-    source_mapping: SourceMapping = None
-    source_mapping_path: str = None
-    pid: int = 0
-    pushed: int = 0
-
-    # Current source:
-    current_title: str | None = None
-    current_page: Dict[str, str] | None = None
-    should_parse: bool = True
-
-    def push(self) -> None:
-        """
-        Pushes data from text representation of wikipedia article into file and mapping structures
-        """
-        if not self.should_parse or self.current_page is None or self.current_title is None:
-            return
-
-        self.pushed += 1
-        for heading, section in self.current_page.items():
-            if is_title_banned(heading):
-                continue
-
-            start_pid = self.pid
-            for paragraph in section.split("\n"):
-                stripped_paragraph = paragraph.strip()
-                if len(stripped_paragraph) > 0:
-                    stripped_paragraph = stripped_paragraph.replace("\t", " ")  # make tsv-safe
-                    self.passage_file.write(f"{self.pid}\t{stripped_paragraph}\n")
-                    self.pid += 1
-
-            clean_title = self.current_title.replace(" ", "_")
-            clean_header = heading.replace(" ", "_").replace("\t", "_").replace("'", "")
-            if "\n" in clean_header:
-                # Header is malformed, remove part of it
-                clean_header = clean_header.split("\n", maxsplit=1)[0]
-            url = f"https://en.wikipedia.org/wiki/{clean_title}#{clean_header}"
-            self.source_mapping.append_interval(self.pid - start_pid, url)
-
-        self.current_title = None
-        self.current_page = None
-
-    def new_page(self):
-        self.push()
-        self.current_title = None
-        self.current_page = None
-        self.should_parse = True
-
-    def reset(self, num: int) -> str:
-        """
-        Creates new files to output passages and source mapping for the passages
-
-        :param num: arbitrary number to distinguish between iterations
-        :return: name of the new file
-        """
-        self.flush()
-        if self.passage_file is not None:
-            self.passage_file.close()
-
-        self.source_mapping_path = os.path.join(self.output_dir, f"{self.collection_name}_sources_{num}.tsv")
-        self.source_mapping = SourceMapping()
-
-        self.passage_file_path = os.path.join(self.output_dir, f"{self.collection_name}_passages_{num}.csv")
-        self.passage_file = open(self.passage_file_path, "w")
-
-        self.pid = 0
-        self.pushed = 0
-
-        return self.passage_file.name
-
-    def flush(self):
-        """
-        Flushes accumulated data to the disk
-        """
-        self.push()
-        if self.passage_file is not None:
-            self.passage_file.flush()
-        if self.source_mapping is not None:
-            self.source_mapping.to_csv(self.source_mapping_path)
-
-    def __tag(self, tag_name):
-        return f"{{{self.namespace}}}{tag_name}" if self.namespace else tag_name
-
-    @property
-    def page_tag(self):
-        return self.__tag("page")
-
-    @property
-    def title_tag(self):
-        return self.__tag("title")
-
-    @property
-    def redirect_tag(self):
-        return self.__tag("redirect")
-
-    @property
-    def text_tag(self):
-        return self.__tag("text")
-
-    @property
-    def ns_tag(self):
-        return self.__tag("ns")
-
-
-def parse_wikitext(text: str) -> Dict[str, str]:
+def parse_wikitext(text: str) -> t.Dict[str, str]:
     """
-    :param text: WikiText, encoding=""
+    :param text: WikiText
     :return: Dictionary of sections, keys are section names and values are section texts
     """
     final_dict = {}
-    wikicode = mw.parse(text, skip_style_tags=True)
+    wikicode = mw.parse(text)
     sections = wikicode.get_sections(flat=True)
     for section in sections:
         headings = section.filter_headings()
@@ -167,7 +55,87 @@ def parse_wikitext(text: str) -> Dict[str, str]:
     return final_dict
 
 
-class PageReporter:
+class SplitCollectionContext:
+    def __init__(self, collection_name: str, path: str):
+        self.collection_name = collection_name
+        self.fid: int = 0
+        self.path = path
+        self.past_cts: t.List[CollectionContext] = []
+        self.last_ctx: CollectionContext | None = None
+
+    def new(self) -> CollectionContext:
+        self.fid += 1
+        self.close()
+        self.last_ctx = CollectionContext(self.collection_name, self.fid, self.path)
+        return self.last_ctx
+
+    def last(self) -> CollectionContext | None:
+        return self.last_ctx
+
+    def close(self):
+        if self.last_ctx is None:
+            return
+        self.last_ctx.close()
+        self.past_cts.append(self.last_ctx)
+        self.last_ctx = None
+
+
+class CollectionContext:
+    class Files(t.NamedTuple):
+        passage_file_path: str
+        mapping_file_path: str
+
+    def __init__(self, collection_name: str, num: int, path: str):
+        self.collection_name = collection_name
+        self.path = path
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        self.passage_file_path = os.path.join(self.path, f"{self.collection_name}_passages_{num}.tsv")
+        self.mapping_file_path = os.path.join(self.path, f"{self.collection_name}_sources_{num}.csv")
+
+        self.passage_file = open(self.passage_file_path, "w")
+        self.mapping = SourceMapping()
+
+        self.pid = 0
+        self.closed = False
+
+    def new_passages(self, title: str, sections: t.Dict[str, str]):
+        for heading, section in sections.items():
+            if is_title_banned(heading):
+                continue
+
+            if "\n" in heading.strip():  # Header is malformed, remove part of it
+                heading = heading.strip().split("\n", maxsplit=1)[0]
+            safe_header = urllib.parse.quote_plus(heading)
+
+            section_start_pid = self.pid
+            for paragraph in section.split("\n"):
+                safe_paragraph = paragraph.strip()
+                if len(safe_paragraph) == 0:
+                    continue
+
+                safe_paragraph = safe_paragraph.replace("\t", " ")  # make tsv-safe
+                self.passage_file.write(f"{self.pid}\t{safe_paragraph}\n")
+                self.pid += 1
+
+            safe_title = urllib.parse.quote_plus(title)
+            url = f"https://en.wikipedia.org/wiki/{safe_title}#{safe_header}"
+            self.mapping.append_interval(self.pid - section_start_pid, url)
+
+    def close(self):
+        if self.closed:
+            return
+        self.closed = True
+        self.passage_file.close()
+        self.mapping.to_csv(self.mapping_file_path)
+
+    def get_files(self) -> Files:
+        return self.Files(self.passage_file_path, self.mapping_file_path)
+
+
+class Reporter:
     last_page: int = 0
 
     @classmethod
@@ -183,48 +151,51 @@ class PageReporter:
             print(f"{page:6d}", end=".")
 
 
-def prepare_wiki(collection_name: str, wiki_path: str, output_dir: str) -> list[str]:
-    print(f"Parsing wiki from file {wiki_path}")
+def prepare_wiki(collection_name: str, path: str, output_dir: str) -> t.List[CollectionContext.Files]:
+    def _page_process(elem: ET.Element):
+        if "ns" not in data or data["ns"] != "0":
+            return
+        if "redirect" in data:
+            return
+        if "title" not in data or is_title_banned(data["title"]):
+            return
 
-    file_num = 1
+        sections = parse_wikitext(elem.text)
+        collection_ctx.last().new_passages(data["title"], sections)
 
-    ctx = WikiParseContext()
-    ctx.collection_name = collection_name
-    ctx.output_dir = output_dir
-    passage_files: List[str] = [ctx.reset(file_num)]
+    a = False
 
-    for event, elem in ET.iterparse(wiki_path, events=("start", "start-ns", "end")):
-        if event == "start-ns":
-            print("new namespace", elem)
-            if elem[0] == "":
-                print(f"set namespace from {ctx.namespace} to {elem[1]}")
-                ctx.namespace = elem[1]
-            continue
+    def _update_context(_):
+        nonlocal collection_ctx
+        if collection_ctx.last_ctx.pid >= 100:
+            collection_ctx.new()
+            nonlocal a
+            if a:
+                raise StopIteration
+            a = True
 
-        if elem.tag == ctx.page_tag:
-            if event == "start":
-                ctx.new_page()
-                PageReporter.report(ctx.pushed)
-        elif elem.tag == ctx.ns_tag:
-            if event == "end" and elem.text != "0":
-                ctx.should_parse = False
-        elif elem.tag == ctx.redirect_tag:
-            ctx.should_parse = False
-        elif elem.tag == ctx.title_tag:
-            if event == "end":
-                ctx.current_title = elem.text
-        elif elem.tag == ctx.text_tag:
-            if event == "end" and ctx.should_parse and not is_title_banned(ctx.current_title):
-                ctx.current_page = parse_wikitext(elem.text)
+    print(f"Parsing wiki from file {path}")
 
-        if ctx.pushed > 25000:
-            print(f"==== pushed {ctx.pushed} passages, creating new file ====", flush=True)
-            file_num += 1
-            passage_files.append(ctx.reset(file_num))
+    collection_ctx = SplitCollectionContext(collection_name, output_dir)
+    collection_ctx.new()
 
-    ctx.push()
-    ctx.flush()
-    return passage_files
+    data: t.Dict[str, str] = {}
+    parse_ctx = ParseContext(path)
+    parse_ctx.collect_contents("ns", data)
+    parse_ctx.collect_contents("redirect", data)
+    parse_ctx.collect_contents("title", data)
+
+    def _clear_data(_):
+        data.clear()
+
+    parse_ctx.end_handler("text", _page_process)
+    parse_ctx.end_handler("page", _clear_data)
+    parse_ctx.end_handler("page", _update_context)
+    parse_ctx.run()
+
+    collection_ctx.close()
+
+    return [ctx.get_files() for ctx in collection_ctx.past_cts]
 
 
 class PrepareWiki(BaseNode):
@@ -233,8 +204,8 @@ class PrepareWiki(BaseNode):
         super().__init__(name, [list], ListDescriptor())
         self.output_dir = output_dir
 
-    def process(self, inp: list[WikiDataFile]) -> list[str]:
-        passage_files: List[str] = list()
+    def process(self, inp: t.List[WikiDataFile]) -> list[str]:
+        passage_files: t.List[str] = list()
         for file in inp:
             pf = prepare_wiki(
                 f"wiki-{file.num}-p{file.p_first}-p{file.p_last}",
